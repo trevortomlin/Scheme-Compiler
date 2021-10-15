4473da6bb0e3ba7d2cee10dc851db8a39595c134
Trevor <trevortomlin2@outlook.com>
2021-10-14 22:27:30 -0700

    Added examples and code in main.

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
 
-int main(){
+int main(int argc, char *argv[]){
 
-    std::cout << "Hello, world!" << std::endl;
+    if (argc != 2){
+
+        std::cout << "Include file to compile." << std::endl;
+        return 1;
+
+    }
+
+    char *file = argv[1];
+
+    std::cout << file << std::endl;
+
 	return 0;
 
 }
