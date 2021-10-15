17c5f9897b3864ae9d5d60393ffc3b9f0947504e
Trevor <trevortomlin2@outlook.com>
2021-10-14 23:01:02 -0700

    Added token struct.

---
+++ include/token.h
@@ -0,0 +1,20 @@
+#ifndef token_h_guard
+#define token_h_guard
+#include <string>
+
+struct token{
+
+    enum token_type{
+        TOKEN_L_PAREN,
+        TOKEN_R_PAREN,
+        TOKEN_EOF,
+    } type;
+
+    std::string value;   
+
+    token(token_type _t, std::string _value);
+
+};
+
+
+#endif
---
+++ src/token.cpp
@@ -0,0 +1,8 @@
+#include "token.h"
+
+token::token(token_type _t, std::string _value){
+
+    type = _t;
+    value = _value;
+    
+}
